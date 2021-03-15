ede17d3e7065f52694cf5b69555c8d35ab1a81f9
GolovinaElia <eliagolovina@ukr.net>
2021-03-15 22:40:27 +0200

    Добавляет файлы

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,19 +1,27 @@
+const  _  =  require( 'lodash' ) ; 
 import countryCard from '../templates/country-items.hbs';
 
 const showCard = document.querySelector('.card-item');
+const inputRef = document.querySelector('#searchQuery');
+
+inputRef.addEventListener('input', _.debounce(onSearch, 500));
+
+function onSearch(event){
+    const searchQuery = event.target.value;
+   
+    fetchCountries(searchQuery)
+    .then(renderCountry)
+    .catch(error => console.log(error))
+}
+
 function fetchCountries(searchQuery) {
-    fetch('https://restcountries.eu/rest/v2/name/Canada')
-        .then(response => {
+    return fetch('https://restcountries.eu/rest/v2/all').then
+        (response => {
             return response.json();
         })
-        .then(country => {
-            console.log(country);
-            const markup = countryCard(country);
-            showCard.innerHTML = markup;
-        })
-        .catch(error => {
-         console.log(error);
-     })
 };
-fetchCountries();
+function renderCountry(country) {
+const markup = countryCard(country);
+showCard.innerHTML = markup;
+}
 export default fetchCountries;
