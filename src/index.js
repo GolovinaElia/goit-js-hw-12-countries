73f1ba1866aca4c67e213b97a0a6d9676666e022
GolovinaElia <eliagolovina@ukr.net>
2021-03-15 23:29:04 +0200

    Добавляет файлы

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 import './styles.css';
+import countryRef from '../src/js/fetchCountries.js';
 
-import '../src/js/fetchCountries.js';
+const _ = require('lodash');
+const inputRef = document.querySelector('#searchQuery');
+inputRef.addEventListener('input', _.debounce(onSearch, 500));
+function onSearch(event){
+    const search = event.target.value;
+//     const filteredCountries = countryRef.filter(country => {
+//         if (search === '') return true;
+
+//         return country.includes(search);
+//     })
+//     console.log(filteredCountries);
+}
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,19 +1,17 @@
-const  _  =  require( 'lodash' ) ; 
+// const  _  =  require( 'lodash' ) ; 
 import countryCard from '../templates/country-items.hbs';
 
 const showCard = document.querySelector('.card-item');
-const inputRef = document.querySelector('#searchQuery');
+// const inputRef = document.querySelector('#searchQuery');
 
-inputRef.addEventListener('input', _.debounce(onSearch, 500));
+// inputRef.addEventListener('input', _.debounce(onSearch, 500));
 
-function onSearch(event){
-    const searchQuery = event.target.value;
-   
-    fetchCountries(searchQuery)
+// function onSearch(event){
+//     const searchQuery = event.target.value;
+// }
+fetchCountries(searchQuery)
     .then(renderCountry)
     .catch(error => console.log(error))
-}
-
 function fetchCountries(searchQuery) {
     return fetch('https://restcountries.eu/rest/v2/all').then
         (response => {
